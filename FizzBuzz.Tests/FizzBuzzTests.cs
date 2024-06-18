3cb02f132f04e7843751f52dc88c66c10cbb7867
Simon Hirst <simonhirst@pm.me>
2024-06-18 14:45:53 +0100

    Added buzz rule test

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -13,5 +13,13 @@ namespace FizzBuzz.UnitTests
             Assert.IsTrue(rule.IsMatch(3));
             Assert.AreEqual("Fizz", rule.GetOutput(3));
         }
+
+        [Test]
+        public void BuzzRule_ReturnsBuzzForMultiplesOfFive()
+        {
+            var rule = new BuzzRule();
+            Assert.IsTrue(rule.IsMatch(5));
+            Assert.AreEqual("Buzz", rule.GetOutput(5));
+        }
     }
 }
