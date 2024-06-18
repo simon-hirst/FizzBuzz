6bfb10d72951bc4f04fe24dd2251c84475b8a796
Simon Hirst <simonhirst@pm.me>
2024-06-18 14:50:13 +0100

    Adding default rule test

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -29,5 +29,12 @@ namespace FizzBuzz.UnitTests
             Assert.IsTrue(rule.IsMatch(15));
             Assert.AreEqual("FizzBuzz", rule.GetOutput(15));
         }
+
+        [Test]
+        public void DefaultRule_ReturnsNumberForNonMultiplesOfThreeOrFive()
+        {
+            var rule = new DefaultRule();
+            Assert.AreEqual("1", rule.GetOutput(1));
+        }
     }
 }
---
+++ FizzBuzz/DefaultRule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class DefaultRule : IFizzBuzzRule
+    {
+        public string GetOutput(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsMatch(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
